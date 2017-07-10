4f74309bda28acab992e4cd3a6e7ea20270d5761
wangyongzhen sam <15950040347@162.com>
2017-07-10 10:17:00 +0800

    init swift project

---
+++ Swift/Demo/Demo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by wangyongzhen sam on 2017/7/10.
+//  Copyright © 2017年 samPersonal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSubviews()
+        
+    }
+
+    private func setupSubviews() {
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
