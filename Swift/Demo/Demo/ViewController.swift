d778c156552e730afd7582b6fa76787c3fd44d9d
wangyongzhen sam <15950040347@162.com>
2017-07-10 11:15:17 +0800

    pod

--- Swift/Demo/Demo/ViewController.swift
+++ Swift/Demo/Demo/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    let tableView = UITableView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupSubviews()
@@ -18,8 +21,27 @@ class ViewController: UIViewController {
 
     private func setupSubviews() {
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
         
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let str = "cell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: str)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: str)
+        }
+        cell?.textLabel?.text = "1234"
+        return cell!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
